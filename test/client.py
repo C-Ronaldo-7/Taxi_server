4666b01492dadc88a6966c5e2ec5f305cd356e27
Glory <gloryhry@stu.xjtu.edu.cn>
2019-06-27 20:36:20 +0800

    在服务器上跟新了互斥锁，注释了需要与数据库进行数据交换的地方。

--- test/client.py
+++ test/client.py
@@ -21,7 +21,7 @@ client_data = {
     "target_position": [1, 1]
 }
 
-
+# 连接服务器
 def doConnect(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -30,7 +30,7 @@ def doConnect(host, port):
         pass
     return sock
 
-
+# 向服务器发送dict数据
 def send_msg(socket, source_data):
     data = json.dumps(source_data)
     data = data + '\n'  # 增加 \n 终止符，用于分割不同次的消息
@@ -40,7 +40,7 @@ def send_msg(socket, source_data):
     print("send msg:%s" % (source_data))
     return True
 
-
+# 从服务器接收json数据，并转为dict类型
 def receive_from_server(sockLocal):
     data_rev = sockLocal.recv(1024).decode()  # 从缓存区中读取并转为str格式
     data_rev = data_rev.split('\n')  # 以\n为终止符，分割出最后收到的一次消息
@@ -50,21 +50,14 @@ def receive_from_server(sockLocal):
     data = json.loads(data_get)  # 转换为json格式
     return data
 
-
+# 循环从服务器上接收数据，断线重连
 def get_data_client():
-    host, port = "192.168.3.18", 50008
+    host, port = "47.96.114.206", 50008
     print(host, port)
     sockLocal = doConnect(host, port)
 
     while True:
         try:
-            # 客户端发送消息并接受服务器的返回值
-            # msg = str(time.strftime("%H:%M:%S"))
-            # msgencode=msg.encode()
-            # sockLocal.sendall(msgencode)
-            # print ("send msg ok : ",msg)
-            # print ("recv data :",sockLocal.recv(1024).decode())
-
             # 服务器主动推送消息
             data = receive_from_server(sockLocal)
             # print(type(data))
@@ -76,22 +69,31 @@ def get_data_client():
             sockLocal = doConnect(host, port)
         except:
             print('other error occur ')
-            time.sleep(3)
-        # time.sleep(1)
+            time.sleep(1)
     sockLocal.shutdown(2)
     sockLocal.close()
 
-
+# 向服务器发送数据,断线重连
 def send_data_client():
-    host, port = "192.168.3.18", 50009
+    host, port = "47.96.114.206", 50009
     sockLocal = doConnect(host, port)
     while True:
-        id = client_data["person_id"]
-        id = int(id)
-        id = id + 1
-        client_data["person_id"] = str(id)
-        send_msg(sockLocal, client_data)
-        time.sleep(1)
+        try:
+            id = client_data["person_id"]
+            id = int(id)
+            id = id + 1
+            client_data["person_id"] = str(id)
+            send_msg(sockLocal, client_data)
+            time.sleep(1)
+        except socket.error:
+            print("ocket error,do reconnect and send data")
+            time.sleep(3)
+            sockLocal=doConnect(host,port)
+        except:
+            print('other error occur, exit')
+            time.sleep(3)
+    sockLocal.shutdown(2)
+    sockLocal.close()
 
 
 if __name__ == "__main__":
--- test/server.py
+++ test/server.py
@@ -2,26 +2,16 @@
 # -*- encoding: utf-8 -*-
 
 # @File    :   server.py
-# @Time    :   2019/06/27 15:09:05
-# @Author  :   Glory Huang
+# @Time    :   2019/06/27 20:33:55
+# @Author  :   Glory Huang 
 # @Contact :   gloryhry@stu.xjtu.edu.cn
-
-# here put the import lib
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-# @File    :   server.py
-# @Time    :   2019/06/24 22:34:56
-# @Author  :   Glory Huang
-# @Contact :   gloryhry@stu.xjtu.edu.cn
-
-# here put the import lib
 
 # here put the import lib
 import socketserver as SocketServer  #导入SocketServer，多线程并发由此类实现
 import json
 import time
 import threading
+from multiprocessing import Lock
 
 # 有关数据交换的相关变量
 # 订单表
@@ -68,8 +58,10 @@ order_data = {
     "start_position": [0, 0],
     "target_position": [1, 1]
 }
+order_lock = Lock()
 
 car_data = {
+    "car_id": 0,
     "current_position": [0, 0],
     "routine": [[0, 0], [1, 1]],
     "velocity": 0.0,
@@ -82,6 +74,7 @@ car_data = {
     "lidar_status": False,
     "ibeo_status": False
 }
+car_lock = Lock()
 
 client_data = {
     "person_id": "0000",
@@ -91,6 +84,7 @@ client_data = {
     "start_position": [0, 0],
     "target_position": [1, 1]
 }
+client_lock = Lock()
 
 
 def send_msg(socket, source_data):
@@ -116,14 +110,9 @@ class send_msg_Server(SocketServer.BaseRequestHandler):  #定义一个类
         print(self.request)
         print(self.client_address)
         while True:
-            # data = self.request.recv(1024)  #需要通过self的方法调用数据接收函数
-            # print(data)
-            # if not data:
-            #     break
-            # datarecv = data.decode()
-            # print("recv data from:%s ;data is:%s" %
-            #       (self.client_address, datarecv))
-            # self.request.send(data.upper())  #需要通过self的方法调用数据接收函数
+            # TODO: 从car_data数据库读取car_data
+
+            # 给客户端发送car_data信息
             if not send_msg(self, car_data):
                 break
             time.sleep(1)
@@ -132,35 +121,78 @@ class send_msg_Server(SocketServer.BaseRequestHandler):  #定义一个类
 class get_client_data_server(SocketServer.BaseRequestHandler):
     def handle(self):
         print('Got a new connection from', self.client_address)
+        #第一次从客户端收到消息
+        get_data = get_msg(self)
+        #添加互斥锁
+        client_lock.acquire()
+        #修改client_data的值
+        for item in get_data.keys():
+            client_data[item] = get_data[item]
+        #互斥锁释放
+        client_lock.release()
+        # TODO: 跟新client_data数据库信息
+
+        # 系统安排车辆
+        # TODO: 从car数据库中找到空闲的车辆。给订单分配车辆
+
+        #更新order_data表单
+        #互斥锁
+        order_lock.acquire()
+        order_data["car_id"] = car_data["car_id"]
+        for item in get_data.keys():
+            if get_data[item] != order_data[item]:
+                order_data[item] = get_data[item]
+        #互斥锁释放
+        order_lock.release()
+        # TODO: 跟新order数据库信息
+
         while True:
-            get_data = get_msg(self) #从客户端获取client_data
+            get_data = get_msg(self)  #从客户端获取client_data
             #添加互斥锁
+            client_lock.acquire()
             #修改client_data的值
             for item in get_data.keys():
-                client_data[item]=get_data[item]
-            #互斥锁
+                client_data[item] = get_data[item]
+            #互斥锁释放
+            client_lock.release()
+            # TODO: 跟新client_data数据库信息
 
+            #更新order_data表单
             #互斥锁
-            # for item in get_data.keys():
-            #     if get_data[item] != order_data[item]:
-            #         order_data[item]=get_data[item]
-            # todo!!!
-            #互斥锁
+            order_lock.acquire()
+            for item in get_data.keys():
+                if get_data[item] != order_data[item]:
+                    order_data[item] = get_data[item]
+            #互斥锁释放
+            order_lock.release()
+            # TODO: 跟新order数据库信息
+
             print(client_data)
 
-def get_client(HOST,PORT):
-    s=SocketServer.ThreadingTCPServer((HOST, PORT), get_client_data_server)
+
+# TODO: 从数据库中跟新car_data表单
+def update_car_data():
+    pass
+
+
+def get_client(HOST, PORT):
+    s = SocketServer.ThreadingTCPServer((HOST, PORT), get_client_data_server)
     s.serve_forever()
 
-def send_client(HOST,PORT):
-    s=SocketServer.ThreadingTCPServer((HOST, PORT), send_msg_Server)
+
+def send_client(HOST, PORT):
+    s = SocketServer.ThreadingTCPServer((HOST, PORT), send_msg_Server)
     s.serve_forever()
 
 
 if __name__ == '__main__':  #并非一定要用这样的方式，只是建议这样使用
     HOST = ''  #定义侦听本地地址口（多个IP地址情况下），这里表示侦听所有
     PORT = 50008  #Server端开放的服务端口
-    get=threading.Thread(target=get_client,args=(HOST,50009))
-    send=threading.Thread(target=send_client,args=(HOST,50008))
+
+    # 由于s.server_forever()会循环执行，所以放入单独一个线程进行循环
+    # 服务器接受数据端口 50009
+    get = threading.Thread(target=get_client, args=(HOST, 50009))
+    # 服务器发送数据端口 50008
+    send = threading.Thread(target=send_client, args=(HOST, 50008))
     get.start()
     send.start()
