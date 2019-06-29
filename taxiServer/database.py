b65d9dbf98b3c6fa945988ccb4180451c4f79f11
glory <gloryhry@stu.xjtu.edu.cn>
2019-06-29 18:10:53 +0800

    完成读写数据库的API

--- taxiServer/database.py
+++ taxiServer/database.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
 # +-----------------------------------------+
-# | @File    :   test.py                    |
-# | @Time    :   2019/06/28 08:58:49        |
+# | @File    :   database.py                |
+# | @Time    :   2019/06/29 14:06:17        |
 # | @Author  :   Glory Huang                |
 # | @Contact :   gloryhry@stu.xjtu.edu.cn   |
 # +-----------------------------------------+
@@ -89,6 +88,54 @@ def update_sql(db, id, id_value, table, data):
     return True
 
 
+# 根据某一主值，读取数据表中信息,返回dict数据
+def read_sql(db, id, id_value, table):
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+    # SQL 查询语句
+    sql = """SELECT * FROM `{table}` WHERE `{id}` = {id_value}""".format(
+        table=table, id=id, id_value=id_value)
+    sql_name = """select COLUMN_NAME,ORDINAL_POSITION from information_schema.COLUMNS where table_name = '{table}' and table_schema = '{database}';""".format(
+        table=table, database="taxi")
+    # print(sql)
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 获取所有记录列表
+        results = cursor.fetchone()
+        # 执行SQL语句
+        cursor.execute(sql_name)
+        columns_name = cursor.fetchall()
+        columns_name = list(columns_name)
+        # 将columns_name元祖转为列表
+        col_name = []
+        for i in range(len(columns_name)):
+            tmp = str(columns_name[i]).split("'")
+            # print(tmp)
+            col_name.append(tmp[1])
+            col_name.append(str(columns_name[i])[-2:-1])
+        # print(col_name)
+        # 先将col_name排序，排成定义的顺序
+        for i in range(0, len(columns_name) - 1):
+            for j in range(0, len(columns_name) - i - 1):
+                if col_name[2 * j + 1] > col_name[2 * j + 3]:
+                    tmp = [col_name[2 * j], col_name[2 * j + 1]]
+                    col_name[2 * j] = col_name[2 * j + 2]
+                    col_name[2 * j + 1] = col_name[2 * j + 3]
+                    col_name[2 * j + 2] = tmp[0]
+                    col_name[2 * j + 3] = tmp[1]
+        # print(col_name)
+        results_dict = {}
+        for i in range(len(results)):
+            col_name[2 * i + 1] = results[i]
+        results_dict = dict(zip(col_name[::2], col_name[1::2]))
+        # print(results_dict)
+    except:
+        print("Error: unable to fetch data")
+        return None
+    return results_dict
+
+
 # 使用预处理语句创建表
 # routine不知道有多长暂定50字节长度
 car_sql = """CREATE TABLE `CAR` (
@@ -164,6 +211,11 @@ if __name__ == "__main__":
     #   "target_position_x": 1.9,
     #   "target_position_y": 1.9
     # }
+    # 跟新某主值为...的数据
     # update_sql(db,"person_id",1,"CLIENT",client_data)
+
+    # 根据主值读取数据库中一行数据
+    # json=read_sql(db,"person_id",1,"CLIENT")
+    # print(json)
 
     db.close()
