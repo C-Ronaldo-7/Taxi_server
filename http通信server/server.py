8bb62dd1b387d62fcf8dc085ca371669beb6bf95
Glory <gloryhry@stu.xjtu.edu.cn>
2019-07-26 17:49:31 +0800

    修改以适应数据库那边的更改，proc-r，idle-a,comp-f

--- http通信server/server.py
+++ http通信server/server.py
@@ -19,10 +19,12 @@ import xinge
 
 app = Flask(__name__)
 
+
 # 404主页
 @app.route('/')
 def hello_world():
     return app.send_static_file('github404.html')
+
 
 # 客户端通信
 @app.route('/client', methods=['POST'])
@@ -31,9 +33,10 @@ def client_post():
     dict_data = json.loads(recv)
     # 更新order数据表
     with order_lock:
-        database.update_sql("order_id", dict_data["order_id"], "order",
-                            dict_data)
-    order_data = database.read_sql("order_id", dict_data["order_id"], "order")
+        database.update_sql("order_id", dict_data["order_id"],
+                            "taxitest_order", dict_data)
+    order_data = database.read_sql("order_id", dict_data["order_id"],
+                                   "taxitest_order")
     # TODO: 判断车辆是否到达接客点，若到达则给客户端推送到达信息 设置account为客户端的手机号
     # push=xinge.XingeApp("a0381c28541e4","570812dd57c4d94451b1e36766db6697")
     # if order_data["is_car_arrive_start"]==Ture and order_data["is_car_reach_target"]==False :
@@ -49,28 +52,30 @@ def client_post():
     #     account = order_data["client_id"]
     #     ret=push.PushSingleAccount(0,account,msg,1)
 
-    if order_data["order_status"] == "comp":  # 当乘客下车后，设置car_status为idle
+    if order_data["order_status"] == "f":  # 当乘客下车后，设置car_status为a
         with car_lock:
             car_data = {}
             car_data["car_id"] = order_data["car_id"]
-            car_data["car_status"] = "idle"
-            database.update_sql("car_id", car_data["car_id"], "car", car_data)
+            car_data["car_status"] = "a"
+            database.update_sql("car_id", car_data["car_id"], "taxitest_car",
+                                car_data)
         with order_lock:
             order_data["order_end_time"] = time.strftime(
                 "%Y-%m-%d %H:%M:%S", time.localtime())
-            database.update_sql("order_id", order_data["order_id"], "order",
-                                order_data)
+            database.update_sql("order_id", order_data["order_id"],
+                                "taxitest_order", order_data)
     order_data["order_creation_time"] = str(order_data["order_creation_time"])
     order_data["order_end_time"] = str(order_data["order_end_time"])
     # TODO: 若需要传送回car的数据,则把car的dict和进order_data
     data = {}
-    car = database.read_sql("car_id", order_data["car_id"], "car")
+    car = database.read_sql("car_id", order_data["car_id"], "taxitest_car")
     for k, v in order_data.items():
         data[k] = v
     for k, v in car.items():
         data[k] = v
     return json.dumps(data)
     # return json.dumps(order_data)
+
 
 # 客户端检查订单
 @app.route('/client/checkorder', methods=['POST'])
@@ -80,10 +85,12 @@ def client_check():
     # print(dict_data)
     # 查询是否有订单正在进行
     order_id = database.find_sql("phone_number", dict_data["phone_number"],
-                                 "order_status", "proc", "order", "order_id")
+                                 "order_status", "r", "taxitest_order",
+                                 "order_id")
     # print(order_id)
     if len(order_id) != 0:  # 如果存在未完成订单，则读取订单数据发送回去
-        order_data = database.read_sql("order_id", order_id[0][0], "order")
+        order_data = database.read_sql("order_id", order_id[0][0],
+                                       "taxitest_order")
         # json格式没法存储datetime类型的数据，要进行转化
         # print(order_data["order_creation_time"])
         order_data["order_creation_time"] = str(
@@ -93,6 +100,7 @@ def client_check():
     else:
         return "当前没有订单"
 
+
 # 客户端创建订单
 @app.route('/client/createorder', methods=['POST'])
 def client_create():
@@ -101,31 +109,32 @@ def client_create():
 
     # 创建新的一个订单
     order_data = {}
-    car = database.read_sql("car_status", "idle", "car")
+    car = database.read_sql("car_status", "a", "taxitest_car")
     if car == None:  # 如果数据库中找不到空闲车辆
         return "当前没有空闲车辆"
     for item in dict_data.keys():
         order_data[item] = dict_data[item]
     # 生成一个19位订单编号
     order_data["order_id"] = createid.get_id(dict_data["phone_number"])
-    order_data["order_status"] = "proc"
+    order_data["order_status"] = "r"
     order_data["car_id"] = car["car_id"]
-    # 更改车辆的状态为proc
-    car["car_status"] = "proc"
+    # 更改车辆的状态为r
+    car["car_status"] = "r"
     with car_lock:
-        database.update_sql("car_id", car["car_id"], "car", car)
+        database.update_sql("car_id", car["car_id"], "taxitest_car", car)
     order_data["order_creation_time"] = time.strftime("%Y-%m-%d %H:%M:%S",
                                                       time.localtime())
     # print(order_data)
     # 写入order_data
     with order_lock:
-        database.write_sql("order", order_data)
+        database.write_sql("taxitest_order", order_data)
     order_data_read = database.read_sql("order_id", order_data["order_id"],
-                                        "order")
+                                        "taxitest_order")
     order_data_read["order_creation_time"] = str(
         order_data_read["order_creation_time"])
     order_data_read["order_end_time"] = str(order_data_read["order_end_time"])
     return json.dumps(order_data_read)
+
 
 # 客户端登录
 @app.route('/client/login', methods=['POST'])
@@ -136,7 +145,8 @@ def client_login():
     # print(dict_data["login_or_create"])
     if dict_data["login_or_create"] == True:
         dict_account = database.read_sql("phone_number",
-                                         dict_data["phone_number"], "client")
+                                         dict_data["phone_number"],
+                                         "taxitest_client")
         # 查询数据库中有无此账号
         if dict_account == None:
             return "无此账户"
@@ -149,13 +159,15 @@ def client_login():
         # 检查数据库中是否有账户
         # print(client.account)
         dict_account = database.read_sql("phone_number",
-                                         dict_data["phone_number"], "client")
+                                         dict_data["phone_number"],
+                                         "taxitest_client")
         if dict_account != None:
             return "已存在此账号，请登录"
         else:
             # print(client.data2dist())
             # print(dict_account)
-            a = database.write_sql("client", dict_data)  # 写client进入数据库
+            a = database.write_sql("taxitest_client",
+                                   dict_data)  # 写client进入数据库
             if a == True:
                 return "Create account success"
             else:
@@ -163,6 +175,7 @@ def client_login():
     else:
         # print(client.login_or_create+"?")
         return "Error occurs"
+
 
 # ROS端通信
 @app.route('/ROS', methods=['POST'])
@@ -193,35 +206,42 @@ def ROS_post():
     # print(order_data)
     # print(dict_data)
     # 判断是否有这辆车的信息，若有则更新这car在数据库中的信息，
-    if database.read_sql("car_id", dict_data["car_id"], "car") == None:
+    if database.read_sql("car_id", dict_data["car_id"],
+                         "taxitest_car") == None:
         return "数据库中没有此车信息，请注册"
     else:
         # 更新这car在数据库中的信息
         if "car_status" in recv_data.keys():
             with car_lock:  #获取car锁
-                database.update_sql("car_id", recv_data["car_id"], "car",
-                                    recv_data)
+                database.update_sql("car_id", recv_data["car_id"],
+                                    "taxitest_car", recv_data)
             return "跟新车辆状态成功"
         else:
             with car_lock:  #获取car锁
-                database.update_sql("car_id", dict_data["car_id"], "car",
-                                    dict_data)
+                database.update_sql("car_id", dict_data["car_id"],
+                                    "taxitest_car", dict_data)
         # 查询当前车辆状态
-        car_data = database.read_sql("car_id", dict_data["car_id"], "car")
-        if car_data["car_status"] == "idle":
+        car_data = database.read_sql("car_id", dict_data["car_id"],
+                                     "taxitest_car")
+        if car_data["car_status"] == "a":
             return "当前没有订单"
-        elif car_data["car_status"] == "proc":
+        elif car_data["car_status"] == "r":
             with order_lock:  # 获取order锁
                 # 跟新order表数据
-                database.update_sql("car_id", order_data["car_id"], "order",
-                                    order_data)
+                database.update_sql("car_id", order_data["car_id"],
+                                    "taxitest_order", order_data)
         else:
             return "Error occurs"  # 车辆既没有在空闲状态，也不在订单状态
     # 得到客户端需要返回的数据
-    order = database.read_sql("car_id", dict_data["car_id"], "order")
+    orderid = database.find_sql("car_id", dict_data["car_id"], "order_status",
+                                "r", "taxitest_order", "order_id")
+    # print(orderid)
+    orderid0 = orderid[0][0]
+    order = database.read_sql("order_id", orderid0, "taxitest_order")
     order["order_creation_time"] = str(order["order_creation_time"])
     order["order_end_time"] = str(order["order_end_time"])
     return json.dumps(order)
+
 
 # ROS端注册
 @app.route('/ROS/register', methods=['POST'])
@@ -243,12 +263,13 @@ def ROS_register():
     }
     # ROS=data_define.ROS_data(dict_data)
     # 查询数据库中是否有ROS.car_id
-    database_ROS = database.read_sql("car_id", dict_data["car_id"], "car")
+    database_ROS = database.read_sql("car_id", dict_data["car_id"],
+                                     "taxitest_car")
     if database_ROS != None:
         return "数据库中已有此车信息，请更新"
     else:
         with car_lock:
-            resulte = database.write_sql("car", dict_data)
+            resulte = database.write_sql("taxitest_car", dict_data)
         if resulte:
             return "车辆注册成功"
         else:
