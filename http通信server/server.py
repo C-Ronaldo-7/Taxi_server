e5ac109d8b36f0cc6aec96aeed75cbbb5c16f511
Jiahui Zhang <2528825706@qq.com>
2019-08-15 17:22:01 +0800

    Update server.py

--- http通信server/server.py
+++ http通信server/server.py
@@ -357,6 +357,7 @@ def ROS_register():
 
 order_lock = threading.Lock()  # 创建order表的锁
 car_lock = threading.Lock()
+client_lock = threading.Lock()
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=False)
 
